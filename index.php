44a0c9527da7cf5ae89dfec83d1f84b7172542f8
Manou10 <84736599+Manou10@users.noreply.github.com>
2021-05-24 21:24:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+ 
+<!doctype html>
+<html >
+    <head>
+  <meta charset="utf-8">
+  <title>RESUME</title>
+ 
+         <link rel="stylesheet" href="resume.css">
+    </head>
+    <body>
+        
+      <h1> <p> <a href="cours.php"> COURS</a></p></h1> 
+      <h1>  <a href="exo.php"> EXERCICE</a></h1> 
+    </body>
+</html>
+
+ 
